271f0ee5baf4e1880e63137f9132b9034f4e9e43
Luei Muazen <luei.muazen@gmail.com>
2022-06-03 17:31:08 +1000

    fix commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Waseem A1
+          Learn React Waseem A2
         </a>
       </header>
     </div>
