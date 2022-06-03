0b95e57c49e55a1b170d3a0bb252c891480d014d
Luei Muazen <luei.muazen@gmail.com>
2022-06-03 17:26:30 +1000

    add text to app.js

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Waseem
+          Learn React Waseem A1
         </a>
       </header>
     </div>
